e858ce8f7966e428c7671cd149dcad6e51e642d8
kocahmet1 <ahmetkoc1@gmail.com>
2025-07-10 17:07:10 +0300

    fix analytcs

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,6 +1,6 @@
 export const trackPageView = (path) => {
   if (window.gtag) {
-    window.gtag('config', 'G-ZYQR480DRN', {
+    window.gtag('config', 'G-VRG24M0NE4', {
       page_path: path,
     });
   }
